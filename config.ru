0a0f8b147b97bdbd08b02bd7d10deaa8c10b5b19
Andrei <ganciandrei@gmail.com>
2012-06-12 02:39:41 +0300

    initial commit

---
+++ config.ru
@@ -0,0 +1,19 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Moonshot::Application
+
+use Rack::Static, 
+  :urls => ["/stylesheets", "/images"],
+  :root => "public"
+
+run lambda { |env|
+  [
+    200, 
+    {
+      'Content-Type'  => 'text/html', 
+      'Cache-Control' => 'public, max-age=86400' 
+    },
+    File.open('public/index.html', File::RDONLY)
+  ]
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moonshot::Application.config.secret_token = '1b15a3ea4cd8c17a7884d52b87ca320d20d1b29df86ed0b894744f7d231d88687d5a4cab3d7d8e8853c6a076e86e9d780eff3dfee5eb63b14183bbf5a80d1cd5'
